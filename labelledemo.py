34a0be4ec24a90d8132de1cf0c5b7143a193e7e3
Biron <tbiron@ensc.fr>
2025-01-24 09:55:08 +0100

    magnifique

--- labelledemo.py
+++ labelledemo.py
@@ -2,6 +2,30 @@ import streamlit as st
 import cv2
 import numpy as np
 
+# CSS pour afficher l'image en haut à droite
+css = """
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+
+.top-right-image {
+    position: absolute;
+    top: 10px; /* Ajustez cette valeur pour la distance par rapport au haut de la page */
+    right: 10px; /* Ajustez cette valeur pour la distance par rapport au bord droit de la page */
+    width: 100px; /* Ajustez cette valeur pour la largeur de l'image */
+    height: auto; /* Conserve les proportions de l'image */
+}
+</style>
+"""
+
+# Intégrer le CSS dans l'application Streamlit
+st.markdown(css, unsafe_allow_html=True)
+
+# Afficher l'image avec la classe CSS
+st.markdown('<img src="https://i.ibb.co/p297XZf/Daphnies.png" alt="Description de l\'image" class="top-right-image">', unsafe_allow_html=True)
+
 # Fonction principale pour traiter la vidéo
 def process_video(video_path, var_threshold, kernel_size, contour_area_threshold):
     # Charger la vidéo
@@ -62,11 +86,21 @@ if uploaded_video is not None:
     with open("uploaded_video.mp4", "wb") as f:
         f.write(uploaded_video.read())
 
-    # Réglages interactifs
-    st.sidebar.header("Paramètres de Détection")
-    var_threshold = st.sidebar.slider("Seuil de variation (varThreshold)", min_value=1, max_value=100, value=6)
-    kernel_size = st.sidebar.slider("Taille du kernel (morphologie)", min_value=1, max_value=20, value=1)
-    contour_area_threshold = st.sidebar.slider("Seuil de surface des contours", min_value=1, max_value=500, value=65)
+    
 
-    # Lancer le traitement vidéo
-    process_video("uploaded_video.mp4", var_threshold, kernel_size, contour_area_threshold)
+    # Boutons pour lancer différentes actions
+    if st.button("Lancer le traitement vidéo"):
+        # Réglages interactifs
+        st.sidebar.header("Paramètres de Détection")
+        var_threshold = st.sidebar.slider("Seuil de variation (varThreshold)", min_value=1, max_value=100, value=6)
+        kernel_size = st.sidebar.slider("Taille du kernel (morphologie)", min_value=1, max_value=20, value=1)
+        contour_area_threshold = st.sidebar.slider("Seuil de surface des contours", min_value=1, max_value=500, value=65)
+        # Lancer le traitement vidéo
+        process_video("uploaded_video.mp4", var_threshold, kernel_size, contour_area_threshold)
+
+    if st.button("Action 2"):
+        st.write("Fonctionnalité 2 en cours de développement")
+
+    if st.button("Action 3"):
+        st.write("Fonctionnalité 3 en cours de développement")
+        st.video("https://youtu.be/KdC0vDLcyAE")
